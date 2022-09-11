40e0b2de68ce9a083710c40b442661ecaf322575
Ole Nascimento <olenascimento@gmail.com>
2022-09-11 23:12:02 +0200

    added react fronend and tailwind index view

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,60 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
 
-const taskIndex = () => {
+const App = () => {
 
-  useEffect(() => {
-    const getData = async () => {
-      try {
+  const [tasks, setTasks] = useState([])
 
-        const res = await axios.get('/api/tasks')
-        console.log('res', res)
+useEffect(() => {
+  const getData = async () => {
+    const { data } = await axios.get('./api/tasks')
+    console.log(data)
+    setTasks(data)
+  }
+ getData()
+},[])
 
-      } catch (err) {
-        console.log(err)
-      }
-    }
-    getData()
-  })
-  
+const handleChange = () => {
+
+}
+
+const taskList = (
+  <>
+  <div className='m-10'>
+    <div>
+      {tasks.map(task => {
+        if (task !== null) {
+          return (
+            <>
+              <div key={task._id} className='grid grid-cols-4 divide-x-2 divide-y-2 '>
+                <input type='checkbox' checked={task.completed} onChange={handleChange} />
+                <p className='bg-slate-700 text-white rounded-sm px-4 py-4'>{task.task}</p>
+                <p className='bg-red-400 text-white rounded-sm px-4 py-4'>{task.createdAt}</p>
+                <button className='bg-slate-700 text-white rounded-sm px-4 py-4'>Details</button>
+              </div>
+            </>
+          )
+        } return null
+      })}
+    </div>
+  </div>
+  <div key='navig'>
+    <input key='add' type="text" />
+    <button key='sub' className='bg-slate-700 text-white rounded-sm px-4 py-4'>Add</button>
+  </div>
+  </>
+)
+
 
 
 
   return (
-    <h1>Hello Task App</h1>
+    <>
+   {taskList}
+    </>
+
   )
 }
 
-export default taskIndex
+export default App
